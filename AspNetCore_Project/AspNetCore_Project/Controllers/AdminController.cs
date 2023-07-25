43280dd01e9ac8a19b7e5d1a5a16984013a486e3
Onur Pıçakcı <98148844+onurpicakci@users.noreply.github.com>
2023-07-25 15:28:44 +0300

    Added skill page for admin section

--- AspNetCore_Project/AspNetCore_Project/Controllers/AdminController.cs
+++ AspNetCore_Project/AspNetCore_Project/Controllers/AdminController.cs
@@ -4,7 +4,27 @@ namespace AspNetCore_Project.Controllers;
 
 public class AdminController : Controller
 {
-    public PartialViewResult SideBar()
+    public PartialViewResult PartialSideBar()
+    {
+        return PartialView();
+    }
+    
+    public PartialViewResult PartialFooter()
+    {
+        return PartialView();
+    }
+    
+    public PartialViewResult PartialNavBar()
+    {
+        return PartialView();
+    }
+    
+    public PartialViewResult PartialHeader()
+    {
+        return PartialView();
+    }
+    
+    public PartialViewResult PartialScripts()
     {
         return PartialView();
     }
--- AspNetCore_Project/AspNetCore_Project/Controllers/SkillController.cs
+++ AspNetCore_Project/AspNetCore_Project/Controllers/SkillController.cs
@@ -1,11 +1,29 @@
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCore_Project.Controllers;
 
 public class SkillController : Controller
 {
-     public IActionResult Index()
-     {
-         return View();
+    SkillManager skillManager = new SkillManager(new EfSkillDal());
+    public IActionResult Index()
+    {
+        var values = skillManager.GetList();
+         return View(values);
      }
+    
+    [HttpGet]
+    public IActionResult AddSkill()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult AddSkill(Skill skill)
+    {
+        skillManager.Add(skill);
+        return RedirectToAction("Index");
+    }
 }
